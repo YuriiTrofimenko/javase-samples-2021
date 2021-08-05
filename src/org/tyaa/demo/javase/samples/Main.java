99306d0f6a6dd9c48260d0b955fc28cdfbe5bf23
YuriiTrofimenko <tyaa@ukr.net>
2021-08-05 11:34:58 +0300

    command line arguments

--- src/org/tyaa/demo/javase/samples/Main.java
+++ src/org/tyaa/demo/javase/samples/Main.java
@@ -5,6 +5,24 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            String action = args[0];
+            switch (action) {
+                case "say-hello": {
+                    System.out.println("Hello!");
+                    break;
+                }
+                case "show-time": {
+                    System.out.println(new Date());
+                    break;
+                }
+                default: {
+                    System.out.println("Unknown argument");
+                }
+            }
+        } catch (Exception ex) {
+            // ex.printStackTrace();
+            System.out.println("No argument");
+        }
     }
 }
